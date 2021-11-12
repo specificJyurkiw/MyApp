87ebe57cfa8e80bece1c21452b9d41d18ca15107
jyurkiw <jyurkiw@specificdx.com>
2021-11-12 12:36:34 -0800

    initial import

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The time is now " + DateTime.Now);
+        }
+    }
+}
